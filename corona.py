f02fabc0920441315d6979b892b9d17c926da4a2
Serge Sivenco <serge@sivenco.su>
2020-04-19 19:10:09 +0300

    0.9.3 Linux adaptation

--- corona.py
+++ corona.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import urllib.request
 from bs4 import BeautifulSoup
 import postgresql
@@ -6,8 +8,7 @@ url_list = [
     'https://xn--80aesfpebagmfblc0a.xn--p1ai/'
 ]
 
-db = postgresql.open('pq://postgres:postgres@localhost:5432/covid19')
-insert = db.prepare("INSERT INTO russia (date, ill, recovered, died, region) VALUES (current_date, $2, $3, $4, $1)")
+connect_string = 'pq://postgres:postgres@localhost:5432/covid19'
 
 all_input = b''
 
@@ -29,17 +30,12 @@ soup = BeautifulSoup(mystr, "html.parser")
 
 div = soup.find('div', class_='d-map__list')
 table = div.find('table')
-for row in table.find_all('tr'):
-    # print(row)
-    state = row.find('th').string
-    # print(state)
-    data = []
-    for column in row.find_all('td'):
-        # for attr in dir(column):
-        #    print("column.%s = %r" % (attr, getattr(column, attr)))
-        # print(column.text)
-        data.append(column.text)
-    # print(data)
-    insert(state, int(data[0]), int(data[1]), int(data[2]))
 
-db.close()
+with postgresql.open(connect_string) as db:
+    insert = db.prepare("INSERT INTO russia (date, ill, recovered, died, region) VALUES (current_date, $2, $3, $4, $1)")
+    for row in table.find_all('tr'):
+        state = row.find('th').string
+        data = []
+        for column in row.find_all('td'):
+            data.append(column.text)
+        insert(state, int(data[0]), int(data[1]), int(data[2]))
